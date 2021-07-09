6790743aed2d1dc3adb3e412780c0109b4883678
Usuariozombie <edupime_98@hotmail.com>
2021-07-10 00:27:34 +0200

    Add files via upload

---
+++ morecrates.lua
@@ -0,0 +1,12 @@
+local PLUGIN = PLUGIN
+
+PLUGIN.name = "More crates"
+PLUGIN.author = "Usuariozombie"
+PLUGIN.description = "You can add boxes using this code, with every single model."
+
+-- ix.container.Register("model", {
+-- 	name = "Box",
+-- 	description = "Description.",
+-- 	width = 4,
+-- 	height = 4,
+-- })
