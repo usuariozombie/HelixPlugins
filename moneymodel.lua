b02097f6d94c4e6f3507542c8294ed86b43b1487
Usuariozombie <edupime_98@hotmail.com>
2021-07-10 00:31:01 +0200

    Add files via upload

---
+++ moneymodel.lua
@@ -0,0 +1,7 @@
+local PLUGIN = PLUGIN
+
+PLUGIN.name = "MoneyChangeModel"
+PLUGIN.author = "Usuariozombie"
+PLUGIN.description = "Simple, it just change tokens model without editing helix plugins."
+
+ix.currency.model = "models/sw_galactic_credits/galactic_credit.mdl"
